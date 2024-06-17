fd657b861dc3c52c871be43bff7618755386d06b
umudovafatima <umudovafatimee@gmail.com>
2024-06-17 12:34:53 +0000

    sallbjn

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char low;
+	char alphabet = 'a';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
 	return (0);
 }
