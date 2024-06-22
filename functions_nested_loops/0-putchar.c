964fd71a35496cd0d49f5b4b64c89a0075ceda08
umudovafatima <umudovafatimeee@gmail.com>
2024-06-22 22:04:15 +0000

    salm

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[] = "_putchar\n";
+
+	int b = 0;
+
+	while (a[b] != '\0')
+	{
+		_putchar(a[b]);
+		b++;
+	}
+	return (0);
+}
