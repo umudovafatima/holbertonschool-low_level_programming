76615725ea886300dbbe27f8e3bca98ad168bebe
umudovafatima <umudovafatimee@gmail.com>
2024-07-07 08:42:43 +0000

    ghj

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * _strchr - Locates a character in a string
- * @s: The string to be searched
- * @c: The character to be located
- *
- * Return: A pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found.
- */
+ * _strchr - locates a character in a string
+ * @c: character that will be looking
+ * @s: string
+ * Return: a pointer to the first occurence of the character c
+ **/
 char *_strchr(char *s, char c)
 {
 	int i;
