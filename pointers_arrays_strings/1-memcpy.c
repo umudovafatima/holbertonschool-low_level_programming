8903dd1c135e00adc6d2b4bdf1aa423cdafce660
umudovafatima <umudovafatimee@gmail.com>
2024-07-07 08:33:29 +0000

    dfgh

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -6,14 +6,14 @@
  * @n: Number of bytes to copy
  * Return: Pointer to dest
  */
-char *_memcpy(char *dest, const char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
 
-    return dest;
+	return (dest);
 }
