c19bc58d87650552a8fa9d9ef6975224edcb3105
umudovafatima <umudovafatimee@gmail.com>
2024-07-07 08:31:46 +0000

    dfgh

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - Copies memory area
+ * @dest: Destination memory area
+ * @src: Source memory area
+ * @n: Number of bytes to copy
+ * Return: Pointer to dest
+ */
+char *_memcpy(char *dest, const char *src, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+
+    return dest;
+}
