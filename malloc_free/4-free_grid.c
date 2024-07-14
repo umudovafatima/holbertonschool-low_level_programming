81148d657023cf86dc939d09717261b91bff1c4b
umudovafatima <umudovafatimee@gmail.com>
2024-07-14 15:07:18 +0000

    fghj

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * free_grid - function for frees a 2 dimensional grid
  *
  * @grid: - the address of the two dimensional grid
  * @height: - height of the grid
- *
- * Return
  */
 void free_grid(int **grid, int height)
 {
