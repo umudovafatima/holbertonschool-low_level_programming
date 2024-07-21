c7d5c47b969b6e6cce1bcba05b03f888ba8370db
umudovafatima <umudovafatimee@gmail.com>
2024-07-21 13:33:55 +0000

    ghjk

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,47 @@
+
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - function that adds two integers
+ * @a: integer
+ * @b: integer
+ * Return: returns the sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - function that subtracs two integers
+ * @a: int
+ * @b: int
+ * Return: returns the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - function that multiplicates two integers
+ * @a: int
+ * @b: int
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
