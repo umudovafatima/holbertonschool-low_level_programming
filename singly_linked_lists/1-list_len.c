78a852afe4d24f66ab650e6adacce183625c9a96
umudovafatima <umudovafatimee@gmail.com>
2024-08-02 18:57:57 +0400

    list_t list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,24 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-
 /**
- * print_list - Main
- * @h: Clone of struct
- * Return: Count of nodes
+ * list_len - main
+ * @h: linkedlist clone
+ * Return: length
  */
-
 
 size_t list_len(const list_t *h)
 {
-  int len = 0;
-  
-  while (h != NULL)
-    {
-    len++;
-    h = h->next;
-  }
-  return (len);
-}
+	int length = 0;
 
+	while (h != NULL)
+	{
+		length++;
+		h = h->next;
+	}
+	return (length);
+}
