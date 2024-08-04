8af110751ae10161b4e3ffbe1bbce8fc8ffd8251
umudovafatima <umudovafatimee@gmail.com>
2024-08-04 13:03:41 +0000

    adds a new node

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+  * add_node_end - add new node to list
+  * @head: start of list
+  * @str: new data
+  * Return: updated list
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *tmp;
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+		return (NULL);
+	temp->str = strdup(str);
+	temp->len = len;
+	temp->next = NULL;
+	tmp = *head;
+	if (*head)
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = temp;
+	}
+	else
+		*head = temp;
+	return (temp);
+}
+
